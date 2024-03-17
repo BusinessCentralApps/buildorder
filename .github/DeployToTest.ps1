a2901afaaeed24b7cd63acc1e0fc9c9afb93ad42
freddydk <freddydk@users.noreply.github.com>
2024-03-17 17:57:42 +0100

    remove dumps

--- .github/DeployToTest.ps1
+++ .github/DeployToTest.ps1
@@ -16,28 +16,16 @@ Param(
 $ErrorActionPreference = "Stop"
 $parameters | ConvertTo-Json -Depth 99 | Out-Host
 
-$parameters.apps | ForEach-Object {
-    Write-Host $_
-    Write-Host $_.GetType()
-    Get-ChildItem -Path $_ -recurse | ForEach-Object { Write-Host "- $($_.FullName)" }
-}
-
-$parameters.apps.GetType()
-$parameters.apps | ForEach-Object {
-    Write-Host "Copy $_"
-    Write-Host $_.GetType()
-}
 $tempPath = Join-Path ([System.IO.Path]::GetTempPath()) ([GUID]::NewGuid().ToString())
 New-Item -ItemType Directory -Path $tempPath | Out-Null
 
 Copy-AppFilesToFolder -appFiles $parameters.apps -folder $tempPath | Out-Null
-$appsList = Get-ChildItem -Path $tempPath -Recurse -Filter *.app
-
+$appsList = Get-ChildItem -Path $tempPath -Filter *.app
 if (-not $appsList -or $appsList.Count -eq 0) {
     Write-Host "::error::No apps to publish found."
     exit 1
 }
 
 Write-Host "Apps:"
-$appsList | ForEach-Object { Write-Host "- $($_.FullName)" }
+$appsList | ForEach-Object { Write-Host "- $($_.Name)" }
 
